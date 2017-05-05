20e41be0506467f89972ce987414ce9b02db9ab8
Andrew Peterson <andrewwaynepeterson@gmail.com>
2017-05-05 15:08:54 -0700

    Added a Town.

---
+++ Town.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Town here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Town extends ParralaxActor
+{
+    Town() {
+        super();
+    }
+    
+    public int getRatio() {
+        return 5;
+    }
+}
